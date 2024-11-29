25ffe1aed800658f009d665285ea0a69b37d21ed
seksatta(labtop) <seksattha@gmail.com>
2024-11-29 11:21:11 +0700

    test 2

--- src/Application.java
+++ src/Application.java
@@ -1,9 +1,8 @@
 public class Application {
     public static void main(String[] args) {
-        for (int i = 0 ; i < 10 ; i++) {
-            PizzaStore NyStore = new NYPizzaStore();
-            NyStore.orderPizza("cheese");
-        }
+
+        PizzaStore NyStore = new NYPizzaStore();
+        NyStore.orderPizza("cheese");
 
         PizzaStore ChicagoStore = new ChicagoPizzaStore();
         ChicagoStore.orderPizza("cheese");
